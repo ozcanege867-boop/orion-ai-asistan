3ea922b55149bcb136cab0f94b50ac30947314bb
ozcanege867-boop <ozcanege867@gmail.com>
2026-02-13 16:23:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pytz
 
 # --- GEMINI AYARI ---
 # Kendi API anahtarını buraya tırnak içine yapıştır
-MY_API_KEY = "AIzaSyDQLCWp_Tq_mg1z9cqT78ABajV6jv5UT7I"
+MY_API_KEY = "AIzaSyCNsC2u199ppNCtPomrdNtoRjbfPcQArSY"
 
 try:
     genai.configure(api_key=MY_API_KEY)
